0626de79206ee31b776d1ca51f7bbfa84a6f6fcb
HosnainRafi <rhosnain@gmail.com>
2021-10-18 11:30:14 +0600

    Team Done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,11 +3,13 @@ import AboutUs from '../../AboutUs/AboutUs';
 import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header';
 import Banner from '../Banner/Banner';
+import Team from '../Team/Team';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Team></Team>
             <AboutUs></AboutUs>
         </div>
     );
--- src/Pages/Home/Team/Team.js
+++ src/Pages/Home/Team/Team.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import doctor from '../../../images/doctor1.jpg'
+import './Team.css'
 
 const Team = () => {
     return (
-        <div className="container">
-            
+        <div className="team">
+            <div className="container container-fluid">
+            <div className="row">
+                <div className="col-md-6 team-title d-flex flex-column justify-content-center">
+                    <h3>Dr. Stephanie Wosniack</h3>
+                    <br />
+                    <h1>Our <span style={{color:'#1d7871', fontWeight:700}}>Team</span></h1>
+                    <br />
+                    <p>Dr. Stephanie Wosniack is dedicated to providing her patients with the best possible care. Her loves in life include her husband, Craig, two cattle dogs, Suture and Kanga, a Malinois, Kiva, and her two horses, Widget and Doc.</p>
+                </div>
+                <div className="col-md-6 img-fluid">
+                    <img className="img-fluid" src={doctor} alt="" />
+                </div>
+            </div>
+        </div>
         </div>
     );
 };
